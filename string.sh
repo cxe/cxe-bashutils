fcb0dec03ea366d460e9d541525005cbb3a18505
Ian Carter <57629805+cxe@users.noreply.github.com>
2025-01-12 22:39:13 +0100

    Update string.sh

--- string.sh
+++ string.sh
@@ -1,9 +1,12 @@
 # @return remove leading and trailing whitespace
 # @arg $1 string The string to be trimmed.
 # @exitcode 2 Function missing arguments.
-string::trim() {
+trim() {
     [[ $# = 0 ]] && printf "%s: Missing arguments\n" "${FUNCNAME[0]}" && return 2
     : "${1#"${1%%[![:space:]]*}"}"
     : "${_%"${_##*[![:space:]]}"}"
     printf '%s\n' "$_"
 }
+
+uppercase(){ local y="$*" && echo "${y^^}"; }
+lowercase(){ local y="$*" && echo "${y,,}"; }
