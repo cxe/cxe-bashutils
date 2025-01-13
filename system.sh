b869cba23a6f413f25d16c95f2ad8bd657dc9aa3
Ian Carter <57629805+cxe@users.noreply.github.com>
2025-01-13 23:27:23 +0100

    Update system.sh

--- system.sh
+++ system.sh
@@ -1,3 +1,19 @@
+#!/usr/bin/env bash
 
-if_sys(){ [ "$OSTYPE" == $1* ] || return 1; }
+# check if OS is linux, win, ios (or specific OS)
+sys_type(){
+  local os="${OSTYPE,,}"
+  case "${1,,}" in
+    mac|darwin|ios) [ $os == darwin* ] && return 0;;
+    linux|unix) if [[ $os == *linux* ]] || [[ $os == *bsd* ]] || [[ $os == *solaris* ]] || [[ $os == *android* ]] || [[ $os == *gnu* ]] then return 0; fi
+    win) if [[ $os == *cygwin* ]] || [[ $os == *mingw* ]] then return 0; fi
+    *) [[ $os == $1* ]] && return 0
+  esac
+  return 1
+}
+
+# OS architecture
 sys_arch(){ uname -m; }
+
+# check if a command is a path-binary
+sys_path_bin(){ builtin type -P "$1" &> /dev/null; }
