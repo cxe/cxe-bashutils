fd213186495e5e62b4828fa695ec290c2b262e4d
Ian Carter <57629805+cxe@users.noreply.github.com>
2025-01-15 14:19:44 +0100

    Update string.sh

--- string.sh
+++ string.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # @return remove leading and trailing whitespace
 # @arg $1 string The string to be trimmed.
 # @exitcode 2 Function missing arguments.
@@ -8,5 +10,55 @@ trim() {
     printf '%s\n' "$_"
 }
 
+# @return uppercase string
 uppercase(){ local y="$*" && echo "${y^^}"; }
+
+# @return lowercase string
 lowercase(){ local y="$*" && echo "${y,,}"; }
+
+# @return a) string, scheme of URL (or "file" if filename exists)
+#         b) if multiple params are given returns 0 only if type of first param matches one of the provided others
+# @param: URL or filename
+# @param(s): URL types to check for match
+# @effect sets global variables: url_type, url_data, url_user, url_pass, url_host, url_port, url_path, url_query
+# @alias is_url parse_url
+# @usage `if url_type "$url" file http ftp; then ...`
+url_type(){
+  local t && declare -gx url_type="" url_user="" url_pass="" url_path="" url_data="$1" url_query="" && shift
+  if [[ "$1" == *?* ]]; then
+    url_data="${1%%$'?'*}"
+    url_query="${1#*$'?'}"
+  fi
+  if [[ "$url_data" == *://* ]]; then
+    url_type="${url_data%%:*}" && url_type="${url_type,,}"
+    url_data="${url_data:((${#url_type}+3))}"
+  elif [ -e "$url_data" ]; then
+    url_type="file"
+  fi
+  case "$url_type" in
+    "") return 1;;
+    file)
+      url_path="$url_data";;
+    http|https|ftp|sftp)
+      url_path="/${url_data#*\/}"
+      url_host="${url_data%%\/*}"
+      if [[ "$url_host" == *@* ]]; then
+        url_user="${url_host%%@*}"
+        if [[ "$url_user" == *:* ]]; then
+          url_pass="${url_user#*:}"
+          url_user="${url_user%%:*}"
+        fi
+        url_host="${url_host#*@}"
+        if [[ "$url_host" == *:* ]]; then
+          url_port="${url_port#*:}"
+        fi
+      fi
+      ;;
+  esac
+  if [ "$#" -eq 0 ]; then
+    echo "$url_type"
+  else
+    for t; do [ "${t,,}" == "$url_type" ] && return; done
+    return 2
+  fi
+}
