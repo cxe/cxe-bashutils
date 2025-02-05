16beeb44b495cf0effe8d709c904fb6f22f69190
Ian Carter <57629805+cxe@users.noreply.github.com>
2025-02-05 11:41:45 +0100

    update fs

---
+++ test/fs.test.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+source "${BASH_SOURCE[0]%/*}/../lib/spec"
+source "${BASH_SOURCE[0]%/*}/../lib/array"
+source "${BASH_SOURCE[0]%/*}/../lib/fs"
+
+
+describe "fs"
+    describe "url_parse"
+        it "should parse a valid URL"
+            declare -A f; url_parse -n f "https://un:pw@Sup.Sub.Example.Com:8443/path/File.ext#Anchor?q=1&l=en"
+            # array_print f
+            expect "${f[query]}" toBe 'q=1&l=en'
+            expect "${f[pass]}" toBe 'pw'
+            expect "${f[file]}" toBe 'file.ext'
+            expect "${f[scheme]}" toBe 'https'
+            expect "${f[path]}" toBe '/path/file.ext'
+            expect "${f[domain]}" toBe 'example.com'
+            expect "${f[port]}" toBe '8443'
+            expect "${f[hostname]}" toBe 'sup.sub.example.com'
+            expect "${f[ext]}" toBe '.ext'
+            expect "${f[tld]}" toBe '.com'
+            expect "${f[frag]}" toBe 'Anchor'
+            expect "${f[host]}" toBe 'sup.sub.example.com:8443'
+            expect "${f[subdomain]}" toBe 'sup.sub'
+            expect "${f[user]}" toBe 'un'
+        ti
+    describe::end
+describe::end
