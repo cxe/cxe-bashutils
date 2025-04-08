80b0c5c82a3bca1c8841a09d190e0560e3ed9656
Ian Carter <57629805+cxe@users.noreply.github.com>
2025-04-08 12:12:08 +0200

    lib/sys_arch

--- src/sys_type.sh
+++ src/sys_type.sh
@@ -5,8 +5,8 @@ sys_type(){
     local cond="${1:-}" && cond="${cond,,}"
     case "$cond" in
         mac|darwin|ios) [[ $os == darwin* ]] ;;
-        linux|unix) [[ $os == *linux* || $os == *bsd* || $os == *solaris* || $os == *android* || $os == *gnu* ]] ;;
-        win) [[ $os == *cygwin* || $os == *mingw* ]] ;;
+        linux|unix|gnu|solaris|android|sunos|bsd) [[ $os == *linux* || $os == *bsd* || $os == *solaris* || $os == *sunos* || $os == *android* || $os == *gnu* ]] ;;
+        win) [[ $os == *cygwin* || $os == *mingw* || $os == *msys* || $os == *windows* || $os == win* ]] ;;
         *) [[ $os == "$cond"* ]] ;;
     esac
     return $?
